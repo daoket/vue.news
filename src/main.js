7781abca28b01c77cce328ddc927a4d057ea761c
nwt <daoket@163.com>
2017-05-05 15:19:32 +0800

    :harmmer: add vuex

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import store from './store'
 import axios from 'axios'
 import VueLazyload from 'vue-lazyload'
 
@@ -22,6 +23,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
---
+++ src/store/SelectStore.js
@@ -0,0 +1,7 @@
+export default {
+  state: {
+    failMessage: '~~~~(>_<)~~~~， 请求到数据失败!',
+    loadBtnText: '点击加载更多',
+    loadBtnStatus: false
+  }
+}
